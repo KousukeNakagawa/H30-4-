d654a27970773bd1ada121a48838e31018a0ae9b
TYasutake <kokoroyuzuka@yahoo.co.jp>
2018-04-12 10:44:13 +0900

    Unityプロジェクト　フォルダ共通するため、最下層に適当なtxtおいてます。後で消します。

---
+++ H30Team04/Assets/Script/MainGameScene/Building/MainStageDate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainStageDate {
+
+    public static float TroutLengthX = 8.0f; //マスの横(x軸)の長さ
+    public static float TroutLengthZ = 8.0f; //マスの縦(z軸)の長さ
+
+    public static float BuildingHeight = 5.0f; //ビルの一階の高さ
+
+
+}
---
+++ H30Team04/Assets/Script/MainGameScene/Building/MapCreate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MapCreate : MonoBehaviour {
+
+    //private TextAsset csvFile; // CSVファイル
+    public TextAsset csvFile; // CSVファイル
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //csvFile = Resources.Load("yonde") as TextAsset;
+        StringReader reader = new StringReader(csvFile.text);
+
+        int num = 0;
+
+        while (reader.Peek() > -1)
+        {
+            num++;
+            //一行読み込む
+            string line = reader.ReadLine();
+            //カンマで区切る
+            string[] nums = line.Split(',');
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Debug.Log("列 " + num + ":" + nums[i]);
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
