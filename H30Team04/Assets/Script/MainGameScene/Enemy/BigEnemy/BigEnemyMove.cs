701198ffa149f28141d160a7e58bd45f2251a708
LAPTOP-8Q2C08VD\g017a1373 <ast.rrs0224@gmail.com>
2018-04-13 14:36:58 +0900

    巨大ロボ追加

---
+++ H30Team04/Assets/Script/MainGameScene/Enemy/BigEnemy/BigEnemyMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigEnemyMove : MonoBehaviour {
+
+    public float x_move = 8.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(x_move * Time.deltaTime, 0, 0);
+	}
+}
