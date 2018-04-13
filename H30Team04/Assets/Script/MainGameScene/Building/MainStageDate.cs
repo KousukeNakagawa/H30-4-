29c6e767be214d14a211191b3137639d4b7ddbc1
TYasutake <kokoroyuzuka@yahoo.co.jp>
2018-04-13 12:21:58 +0900

    マスの大きさ更新

--- H30Team04/Assets/Script/MainGameScene/Building/MainStageDate.cs
+++ H30Team04/Assets/Script/MainGameScene/Building/MainStageDate.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MainStageDate {
 
-    public static float TroutLengthX = 8.0f; //マスの横(x軸)の長さ
-    public static float TroutLengthZ = 8.0f; //マスの縦(z軸)の長さ
+    public static float TroutLengthX = 10.0f; //マスの横(x軸)の長さ
+    public static float TroutLengthZ = 10.0f; //マスの縦(z軸)の長さ
 
     public static float BuildingHeight = 5.0f; //ビルの一階の高さ
+    public static float BuildingEdge = 8.0f; //ビルの辺の長さ
 
 
 }
