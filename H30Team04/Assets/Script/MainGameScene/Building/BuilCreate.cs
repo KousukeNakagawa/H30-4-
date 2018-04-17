5b27216742f77d72cc83eb82db3901697a1cf55b
TYasutake <kokoroyuzuka@yahoo.co.jp>
2018-04-17 11:02:25 +0900

    csvからのマップ作製　射影機のオブジェクト

---
+++ H30Team04/Assets/Script/MainGameScene/Building/BuilCreate.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuilCreate : MonoBehaviour {
+
+    public GameObject centerPrefab;
+    public GameObject topPrefab;
+
+    public int centerCount = 1;
+    private int builHeightCount = 1;  //ビルを作るときの今何階建てか
+
+    private BoxCollider m_Collider;
+    private Transform underBuil;
+
+	// Use this for initialization
+	void Start () {
+        m_Collider = GetComponent<BoxCollider>();
+        underBuil = transform.Find("builunder");
+        int builSize = 1;
+
+        Vector3 newPos;
+
+        //中央部分作成
+        for (int i = 0; i < centerCount; i++)
+        {
+            GameObject center = Instantiate(centerPrefab);
+            center.transform.parent = transform;
+            newPos = new Vector3(0,builSize * MainStageDate.BuildingHeight,0) + underBuil.localPosition;
+            builSize++;
+            center.transform.localPosition = newPos;
+            center.transform.rotation = transform.rotation;
+        }
+
+        //屋上作成
+        GameObject top = Instantiate(topPrefab);
+        top.transform.parent = transform;
+        newPos = new Vector3(0, builSize * MainStageDate.BuildingHeight, 0) + underBuil.localPosition;
+        builSize++;
+        top.transform.localPosition = newPos;
+        top.transform.rotation = transform.rotation;
+
+        //あたり判定の設定
+        m_Collider.size = new Vector3(m_Collider.size.x, builSize * MainStageDate.BuildingHeight, m_Collider.size.z);
+        m_Collider.center = new Vector3(m_Collider.center.x, (builSize * MainStageDate.BuildingHeight)/2, m_Collider.center.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetCenterCount(string count)
+    {
+        int num = int.Parse(count);
+        num -= 2;
+        centerCount = num;
+    }
+}
--- H30Team04/Assets/Script/MainGameScene/Building/MapCreate.cs
+++ H30Team04/Assets/Script/MainGameScene/Building/MapCreate.cs
@@ -8,6 +8,13 @@ public class MapCreate : MonoBehaviour {
     //private TextAsset csvFile; // CSVファイル
     public TextAsset csvFile; // CSVファイル
 
+    public GameObject builAPrefab;
+    public GameObject builBPrefab;
+    public GameObject builCPrefab;
+    public GameObject XrayMachinePrefab;
+
+    private int row = 0;
+
 
     // Use this for initialization
     void Start()
@@ -16,19 +23,34 @@ public class MapCreate : MonoBehaviour {
         //csvFile = Resources.Load("yonde") as TextAsset;
         StringReader reader = new StringReader(csvFile.text);
 
-        int num = 0;
 
         while (reader.Peek() > -1)
         {
-            num++;
             //一行読み込む
             string line = reader.ReadLine();
             //カンマで区切る
-            string[] nums = line.Split(',');
-            for (int i = 0; i < nums.Length; i++)
+            string[] data = line.Split(',');
+
+
+            for (int i = 0; i < data.Length; i++)
             {
-                Debug.Log("列 " + num + ":" + nums[i]);
+                //-で区切る
+                string[] minidata = data[i].Split('-');
+
+                if (minidata.Length < 2) continue;
+
+                if(minidata[0] == "X") //射影機
+                {
+                    GameObject createObject = Instantiate(XrayMachinePrefab);
+                    createObject.transform.position = ObjectPosition(minidata[0], minidata[1], i);
+                    createObject.transform.rotation = Quaternion.AngleAxis(RotationSize(minidata[1]), new Vector3(0, 1, 0));
+                }
+                else { 
+                    Arrangement(minidata[0], minidata[1], minidata[2], i);
+                }
             }
+
+            row++;
         }
 
 
@@ -38,5 +60,106 @@ public class MapCreate : MonoBehaviour {
     void Update()
     {
 
+    }
+
+    private void Arrangement(string name,string centercount, string direction, int column)
+    {
+        //生成するビル
+        GameObject createObject = null;
+
+        switch (name)
+        {
+            case "A": createObject = Instantiate(builAPrefab); break;
+            case "B": createObject = Instantiate(builBPrefab); break;
+            case "C": createObject = Instantiate(builCPrefab); break;
+        }
+
+        if (createObject == null) return;
+
+        //ビルの中央の数
+        createObject.GetComponent<BuilCreate>().SetCenterCount(centercount);
+
+        //ビルのポジション
+        createObject.transform.position = ObjectPosition(name, direction, column);
+
+        //ビルの回転
+        createObject.transform.rotation = Quaternion.AngleAxis(RotationSize(direction), new Vector3(0, 1, 0));
+
+    }
+
+    private Vector3 ObjectPosition(string name, string direction, int column)
+    {
+        Vector3 result = Vector3.zero;
+
+        if(direction == "E") //東向き
+        {
+            switch (name)
+            {
+                case "A":
+                case "B":
+                case "X":
+                    result.x = (column + 1) * MainStageDate.TroutLengthX;
+                    result.z = -row * MainStageDate.TroutLengthZ;
+                    break;
+                case "C":
+                    result.x = (column + 2) * MainStageDate.TroutLengthX;
+                    result.z = -row * MainStageDate.TroutLengthZ;
+                    break;
+            }
+        }
+        else if (direction == "S") //南向き
+        {
+            switch (name)
+            {
+                case "A":
+                case "X":
+                    result.x = (column + 1) * MainStageDate.TroutLengthX;
+                    result.z = -(row + 1) * MainStageDate.TroutLengthZ;
+                    break;
+                case "B":
+                    result.x = (column + 2) * MainStageDate.TroutLengthX;
+                    result.z = -(row + 1) * MainStageDate.TroutLengthZ;
+                    break;
+                case "C":
+                    result.x = (column + 2) * MainStageDate.TroutLengthX;
+                    result.z = -(row + 2) * MainStageDate.TroutLengthZ;
+                    break;
+            }
+        }
+        else if(direction == "W") //西向き
+        {
+            switch (name)
+            {
+                case "A":
+                case "X":
+                    result.x = column * MainStageDate.TroutLengthX;
+                    result.z = -(row + 1) * MainStageDate.TroutLengthZ;
+                    break;
+                case "B":
+                case "C":
+                    result.x = column * MainStageDate.TroutLengthX;
+                    result.z = -(row + 2) * MainStageDate.TroutLengthZ;
+                    break;
+            }
+        }
+        else //北向き
+        {
+            result.x = column * MainStageDate.TroutLengthX;
+            result.z = -row * MainStageDate.TroutLengthZ;
+        }
+
+        return result;
+    }
+
+    private float RotationSize(string direction)
+    {
+        //建物の回転
+        float rotation = 0;
+
+        if (direction == "E") rotation = 90;
+        else if (direction == "S") rotation = 180;
+        else if (direction == "W") rotation = 270;
+
+        return rotation;
     }
 }
