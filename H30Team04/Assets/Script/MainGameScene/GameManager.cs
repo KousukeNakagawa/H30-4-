49ee9415a68cfa516ab047db06f37f38e40729b1
DESKTOP-4T69V3H-MIRIA <kousuke.shinan@gmail.com>
2018-04-21 20:19:06 +0900

    ゲームマネージャー

--- H30Team04/Assets/Script/MainGameScene/GameManager.cs
+++ H30Team04/Assets/Script/MainGameScene/GameManager.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+    enum PhaseState
+    {
+        photoState,
+        attackState
+    }
 
+    private PhaseState phaseState;
     [SerializeField]
     GameObject m_Player;
     [SerializeField]
@@ -17,18 +24,69 @@ public class GameManager : MonoBehaviour {
     [SerializeField]
     bool test = false;  //必ず消し去るbool型
 
+    GameObject Flayers;
+    GameObject Arrows;
+    public int m_FlyerCount;
+    bool m_IsChange;
+    private int currentSelectStageIndex;
 
     // Use this for initialization
     void Start () {
-        
-	}
+        phaseState = PhaseState.photoState;
+        currentSelectStageIndex = 0;
+        Flayers = transform.Find("Screen").Find("Flyers").gameObject;
+        Arrows = Flayers.transform.Find("Arrows").gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         m_MiniMap.transform.position = new Vector3(m_Player.transform.position.x, m_Player.transform.position.y + cameraH, m_Player.transform.position.z);
+        UpdateSelect();
+    }
+    void FixedUpdate()
+    {
+        switch (phaseState)
+        {
+            case PhaseState.photoState: PhotoState(); break;
+            case PhaseState.attackState: AttackState(); break;
+        }
+    }
+    void UpdateSelect()
+    {
+        const float Margin = 0.5f;
+        float inputHorizontal = (Input.GetAxisRaw("XboxLeftHorizontal") != 0) ? Input.GetAxisRaw("XboxLeftHorizontal") : Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(inputHorizontal) > Margin && !m_IsChange)
+        {
+            if (inputHorizontal > 0.0f)
+            {
+                currentSelectStageIndex += 1;
+            }
+            else
+            {
+                currentSelectStageIndex += (m_FlyerCount - 1);
+            }
+            currentSelectStageIndex = currentSelectStageIndex % m_FlyerCount;
+            float l_positionX = currentSelectStageIndex * 1280;
+            //Arrows.GetComponent<Arrows>().SetTargetLocalPositionX(l_positionX);
+            //Flayers.GetComponent<Flyers>().MoveTargetPositionX(-l_positionX);
+            m_IsChange = true;
+        }
+        else if (Mathf.Abs(inputHorizontal) <= Margin)
+        {
+            m_IsChange = false;
+        }
+    }
+
+        private void PhotoState()
+    {
         GameClear();
         GameOver();
         PhaseTransition();
+    }
+
+    private void AttackState()
+    {
+
     }
 
     void GameClear()
@@ -51,7 +109,7 @@ public class GameManager : MonoBehaviour {
     {
         if (test)
         {
-            Debug.Log("防衛ラインに侵入した");
+            phaseState = PhaseState.attackState;
         }
     }
 }
