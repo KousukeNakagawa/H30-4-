aa09e452b6cd8a12c9b62310f5ec23536a013a18
tur.kuma4524@gmail.com <tur.kuma4524@gmail.com>
2018-05-17 10:35:56 +0900

    タイトル

---
+++ H30Team04/Assets/Script/Other/Title/Hilyouzi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hilyouzi : MonoBehaviour
+{
+
+    public GameObject gameObj;
+    public GameObject gameObj2;
+    // Use this for initialization
+    void Start()
+    {
+        gameObj.SetActive(false);
+        gameObj2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetButtonDown("Shutter"))
+        {
+            gameObj.SetActive(true);
+            gameObj2.SetActive(true);
+        }
+    }
+}
---
+++ H30Team04/Assets/Script/Other/Title/TiteleText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiteleText : MonoBehaviour
+{
+
+    private GameObject textObj;
+
+    private float nextTime;
+    public float interval = 0.8f;//点滅時間
+
+    // Use this for initialization
+    void Start()
+    {
+        textObj = GameObject.Find("PushA");
+        nextTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextTime)
+        {
+            float alpha = textObj.GetComponent<CanvasRenderer>().GetAlpha();
+            if (alpha == 1.0f)
+            {
+                textObj.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
+            }
+            else
+            {
+                textObj.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
+            }
+            nextTime += interval;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Shutter"))
+        {
+            Destroy(textObj);
+        }
+    }
+}
