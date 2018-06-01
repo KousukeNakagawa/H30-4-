2cb13a1708963465f62d525f0f2e8bd3dfbfe2bf
LAPTOP-8Q2C08VD\g017a1373 <ast.rrs0224@gmail.com>
2018-06-01 11:12:16 +0900

    ステージの倒れるオブジェクト追加

--- H30Team04/Assets/Script/MainGameScene/StageObject/BreakDown.cs
+++ H30Team04/Assets/Script/MainGameScene/StageObject/BreakDown.cs
@@ -7,6 +7,7 @@ public class BreakDown : MonoBehaviour
     private Rigidbody m_rigid;
     private Vector3 primaryPos;
     [SerializeField, Range(0.0f, 5.0f)] private float torquePower = 2.0f;
+    private bool isHit = false;
 
     // Use this for initialization
     void Start()
@@ -23,6 +24,7 @@ public class BreakDown : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
+        if (isHit) return;
         if (other.gameObject.CompareTag("Player"))
         {
             BreakDownAction(other.transform);
@@ -30,9 +32,10 @@ public class BreakDown : MonoBehaviour
     }
     public void BreakDownAction(Transform target)
     {  //プレイヤーと当たらないレイヤーに変更
-        gameObject.layer = LayerMask.NameToLayer("StageObject");
-        Vector3 dir = -target.forward;
+        //gameObject.layer = LayerMask.NameToLayer("StageObject");
+        Vector3 dir = -(target.position - transform.position).normalized;
         m_rigid.useGravity = true;
-        m_rigid.AddTorque(dir * torquePower, ForceMode.VelocityChange);
+        m_rigid.AddForce(dir * torquePower, ForceMode.VelocityChange);
+        isHit = true;
     }
 }
---
+++ H30Team04/Assets/Script/MainGameScene/StageObject/DropDown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDown : MonoBehaviour
+{
+    private bool isFirst = false;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Building")
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
