aa3d17a27b88c1c6cd4183ae8f4d0be316d75336
G017A1281-山崎駿 <kuroeshi0123@gmail.com>
2018-06-01 13:35:46 +0900

    ごめんなさい

--- H30Team04/Assets/Script/MainGameScene/Player/Soldier.cs
+++ H30Team04/Assets/Script/MainGameScene/Player/Soldier.cs
@@ -31,6 +31,29 @@ public class Soldier : MonoBehaviour
     bool isLaserHit = false;
     float laserLength;
 
+
+
+    //開始演出終了ポイント
+    [SerializeField] Transform endSEPoint;
+    //通常速度
+    [SerializeField, Range(0.1f, 10f)] float walkSpeed = 2f;
+    //ダッシュ時速度
+    [SerializeField, Range(0.1f, 100f)] float dashSpeed = 6f;
+    //旋回力
+    [SerializeField, Range(0.1f, 30f)] float rotateSpeed = 2;
+    //死亡可能回数
+    [SerializeField] int residue = 3;
+
+
+    //リスポーン用
+    Vector3 startPosition;
+    Quaternion startRotation;
+
+    bool _isEndSE = false; //開始演出の終了フラグ
+    bool _isMove = true; //開始演出運転フラグ
+    //死亡判定
+    bool _isDead = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -179,5 +202,104 @@ public class Soldier : MonoBehaviour
     public bool GetWeapon()
     {
         return isWeaponBeacon;
+    }
+
+    /// <summary>
+    /// リスポーン
+    /// </summary>
+    public void Respawn()
+    {
+        if (Annihilation()) return;
+
+        if (_isDead)
+        {
+            //移動を殺す
+            rb.velocity = Vector3.zero;
+
+            if (Input.GetButtonDown("Select"))
+            {
+                //初期位置へ
+                transform.position = startPosition;
+                transform.rotation = startRotation;
+                //ビッグエネミーを向く
+                transform.LookAt(BigEnemyScripts.mTransform);
+                residue--; //死亡可能回数の減少
+                _isDead = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 死亡判定
+    /// </summary>
+    public bool IsDead()
+    {
+        return _isDead;
+    }
+
+    /// <summary>
+    /// ＊残機が0になったら「全滅」＝True
+    /// </summary>
+    public bool Annihilation()
+    {
+        return (residue <= 0) ? true : false;
+    }
+
+    /// <summary>
+    /// ＊死亡
+    /// </summary>
+    void Death()
+    {
+        //カメラは破壊しない
+        Camera.main.transform.parent = null;
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+    }
+
+    /// <summary>
+    /// 開始演出の自動運転（X軸）
+    /// </summary>
+    void SEMove_X()
+    {
+        if (_isEndSE) return;
+
+        //移動量
+        Vector3 move = new Vector3(endSEPoint.position.x - transform.position.x, 0);
+
+        //移動（目的地に近づくほど減速）
+        if (_isMove) transform.position += new Vector3(move.normalized.x / 10 + move.x / 100, 0);
+
+        //開始演出の終了
+        else _isEndSE = true;
+
+        //到着
+        if (Mathf.Abs(move.x) < 1) _isMove = false;
+    }
+
+    /// <summary>
+    /// 開始演出の自動運転（Z軸）
+    /// </summary>
+    void SEMove_Z()
+    {
+        if (_isEndSE) return;
+
+        //移動量
+        Vector3 move = new Vector3(endSEPoint.position.z - transform.position.z, 0);
+
+        //移動（目的地に近づくほど減速）
+        if (_isMove) transform.position += new Vector3(move.normalized.z / 10 + move.z / 100, 0);
+
+        //開始演出の終了
+        else _isEndSE = true;
+
+        //到着
+        if (Mathf.Abs(move.z) < 1) _isMove = false;
+    }
+
+    /// <summary>
+    /// 開始演出が終わったか
+    /// </summary>
+    public bool GetIsEndSE()
+    {
+        return _isEndSE;
     }
 }
