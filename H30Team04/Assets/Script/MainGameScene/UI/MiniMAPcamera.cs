ff14327ed12d3dfa53565f87c510a77ba3651c86
DESKTOP-4T69V3H-MIRIA <kousuke.shinan@gmail.com>
2018-06-02 12:02:09 +0900

    ポーズ時のマップ

--- H30Team04/Assets/Script/MainGameScene/UI/MiniMAPcamera.cs
+++ H30Team04/Assets/Script/MainGameScene/UI/MiniMAPcamera.cs
@@ -10,11 +10,23 @@ public class MiniMAPcamera : MonoBehaviour
     [SerializeField]
     float cameraH = 20;
     Rect _rect = new Rect(0, 0, 1, 1);
+    Camera m_camera;
+    [SerializeField]
+    RenderTexture m_minimapRender;
+    [SerializeField]
+    RenderTexture m_mapRender;
+
+    [SerializeField]
+    GameObject m_Minimap;
+    [SerializeField]
+    GameObject m_Map;
+    
+    public bool _pose = false;
 
     // Use this for initialization
     void Start()
     {
-        
+        m_camera = GetComponent<Camera>();
     }
     private void FixedUpdate()
     {
@@ -24,14 +36,23 @@ public class MiniMAPcamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(m_Player.transform.position.x, m_Player.transform.position.y + cameraH, m_Player.transform.position.z);
-        transform.rotation = Quaternion.Euler(90, 0, 0);
-
+        if (!_pose)
+        {
+            m_Map.SetActive(false);
+            m_Minimap.SetActive(true);
+            m_camera.targetTexture = m_minimapRender;
+            m_camera.orthographicSize = 25;
+            transform.position = new Vector3(m_Player.transform.position.x, m_Player.transform.position.y + cameraH, m_Player.transform.position.z);
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+        else
+        {
+            m_Minimap.SetActive(false);
+            m_Map.SetActive(true);
+            m_camera.targetTexture = m_mapRender;
+            m_camera.orthographicSize = 200;
+            transform.position = new Vector3(191, 145, -111);
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
     }
-
-    //public Rect MiniCameraRect
-    //{
-    //    get { return _rect; }
-    //    set { _rect = value; }
-    //}
 }
--- H30Team04/Assets/Script/MainGameScene/UI/XlineMarker.cs
+++ H30Team04/Assets/Script/MainGameScene/UI/XlineMarker.cs
@@ -8,15 +8,15 @@ public class XlineMarker : MonoBehaviour
     Xray_SSS m_xsss;
     int XlineCount;
     bool test=false;
+    [SerializeField]
     GameObject m_mainCamera;
 
     // Use this for initialization
     void Start()
     {
-        m_mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+      //  m_mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         m_player = GameObject.FindGameObjectWithTag("Player");
         m_xsss = m_player.GetComponent<Xray_SSS>();
-
     }
 
     // Update is called once per frame
