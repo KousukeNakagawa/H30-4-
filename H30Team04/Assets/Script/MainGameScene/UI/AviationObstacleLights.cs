38d8847bd90227a402836a22a0700467d6704a1c
DESKTOP-4T69V3H-MIRIA <kousuke.shinan@gmail.com>
2018-06-23 15:42:17 +0900

    博士のレイヤーとサカイタワー

---
+++ H30Team04/Assets/Script/MainGameScene/UI/AviationObstacleLights.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AviationObstacleLights : MonoBehaviour {
+
+    GameObject[] m_Lights;
+
+    // Use this for initialization
+    void Start() {
+        m_Lights = GameObject.FindGameObjectsWithTag("AviationLight");
+        StartCoroutine("Light");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator Light()
+    {
+        while (true)
+        {
+            foreach (GameObject _lights in m_Lights)
+            {
+                _lights.GetComponent<MeshRenderer>().enabled = !_lights.GetComponent<MeshRenderer>().enabled;
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
