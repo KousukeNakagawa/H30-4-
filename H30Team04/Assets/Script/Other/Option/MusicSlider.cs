ff15982d71bd277bd718ff5105bad96d6848772b
tur.kuma4524@gmail.com <tur.kuma4524@gmail.com>
2018-07-17 13:32:18 +0900

    オプション画面

---
+++ H30Team04/Assets/Script/Other/Option/MusicSlider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSlider : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public UnityEngine.Audio.AudioMixer mixer;
+
+
+
+
+    public void masterVol(Slider slider)
+    {
+        mixer.SetFloat("Master", slider.value);
+    }
+
+    public void BGMVol(Slider slider1)
+    {
+        mixer.SetFloat("BGM", slider1.value);
+    }
+    public void SEVol(Slider slider2)
+    {
+        mixer.SetFloat("SE", slider2.value);
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ H30Team04/Assets/Script/Other/Option/Sensitivity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Sensitivity : MonoBehaviour {
+
+    private Slider slider;
+    public static float Sensitivitytest;
+    [SerializeField]
+    public GameObject gameObj;
+
+
+    public static float getSensitivitytest()
+    {
+        return Sensitivitytest;
+    }
+    // Use this for initialization2
+    void Start () {
+        slider = GetComponent<Slider>();
+        EventSystem.current.SetSelectedGameObject(gameObj);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Sensitivitytest = slider.value;
+        if(Input.GetMouseButton(0))
+        {
+            EventSystem.current.SetSelectedGameObject(gameObj);
+        }
+    }
+   
+}
